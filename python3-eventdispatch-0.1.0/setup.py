7db7c7dad174cf0474dd12be672896ef125b7015
Charlie Yan <charlieyan1@meta.com>
2025-05-23 23:48:11 -0700

    debian build dependencies

--- python3-eventdispatch-0.1.0/setup.py
+++ python3-eventdispatch-0.1.0/setup.py
@@ -6,7 +6,7 @@ setup(
     description='Event Dispatch, a discrete time synchronizer',
     url='http://github.com/cyan-at/eventdispatch',
     author='Charlie Yan',
-    author_email='charlieyan08@gmail.com',
+    author_email='cyanatg@gmail.com',
     license='Apache-2.0',
     install_requires=['requests'],
     packages=find_packages(),
