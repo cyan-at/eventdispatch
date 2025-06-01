7ce4bbdd60789ad3e8637b5041a55ca30c61d604
Charlie Yan <charlieyan1@meta.com>
2025-05-31 17:17:28 -0700

    0.1.18

--- python3/apt_toolchain.sh
+++ python3/apt_toolchain.sh
@@ -2,4 +2,4 @@
 
 debuild -k2024A1A77FE666D2A742FEDA6EE9B8235B1719DD -S
 
-dput ppa:cyanatlaunchpad/python3-eventdispatch-ppa ../python3-eventdispatch_0.1.17_source.changes
+dput ppa:cyanatlaunchpad/python3-eventdispatch-ppa ../python3-eventdispatch_0.1.18_source.changes
